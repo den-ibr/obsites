f5612744acacd879834ca3686209f9a7fa514e95
den-ibr <ibr.den.dan@gmail.com>
2025-05-20 02:30:02 +0500

    а

--- telegram_user.py
+++ telegram_user.py
@@ -8,21 +8,29 @@ BOT_TOKEN = getenv('BOT_TOKEN')
 
 
 class TelegramUser(BaseModel):
-    auth_date: str  # ?
+    auth_date: int
     first_name: str
-    id: str  # ?
+    id: int
     username: str
     hash: str
 
     @model_validator(mode='before')
     def check_hash(cls, values: dict[str, any]) -> dict[str, any]:
-        data_check_keys = sorted(
-            k for k in values.keys() if k != 'hash' and values[k] is not None
-        )
-        data_check_string = '\n'.join(f"{k}={values[k]}" for k in data_check_keys)
-        secret_key = hashlib.sha256(BOT_TOKEN.encode()).digest()
-        computed_hash = hmac.new(secret_key, data_check_string.encode(), hashlib.sha256).hexdigest()
-        if computed_hash != values.get('hash'):
-            raise ValueError(f'Invalid login data, {computed_hash} != {values.get('hash')}')
-        print('Успех!')
-        return values
+        try:
+            data_check_keys = sorted(
+                k for k in values.keys() if k != 'hash' and values[k] is not None
+            )
+            data_check_string = '\n'.join(
+                f"{k}={str(values[k])}" for k in data_check_keys
+            )
+
+            secret_key = hashlib.sha256(BOT_TOKEN.encode()).digest()
+            computed_hash = hmac.new(secret_key, data_check_string.encode(), hashlib.sha256).hexdigest()
+            provided_hash = values.get('hash')
+
+            if not hmac.compare_digest(computed_hash, provided_hash):
+                raise ValueError(f'Invalid login data, {computed_hash} != {provided_hash}')
+
+            return values
+        except Exception as e:
+            raise ValueError(f"Failed to validate Telegram data: {e}")
