0f17388d236b7979819b254706acf808df308096
den-ibr <ibr.den.dan@gmail.com>
2025-06-19 14:35:17 +0500

    Add 404 message when note cannot be found

--- static/note.js
+++ static/note.js
@@ -1,10 +1,11 @@
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get('id');
 
-while (!window.MathJax) { }
+while (!window.MathJax) {}
 fetch(`https://obsites-api.vercel.app/files/${id}`)
     .then((response) => {
         if (!response.ok) {
+            document.getElementById('title').innerText = '404: Note not found';
             throw new Error(`HTTP error! status: ${response.status}`);
         }
         return response.text();
